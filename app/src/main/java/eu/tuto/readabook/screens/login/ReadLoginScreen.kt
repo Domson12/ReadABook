b46c77c9a50c5779d46c72719b44a414574b1802
Domson12 <dominikmz12@op.pl>
2022-12-06 14:26:31 +0100

    login screen input fields and button

--- app/src/main/java/eu/tuto/readabook/screens/login/ReadLoginScreen.kt
+++ app/src/main/java/eu/tuto/readabook/screens/login/ReadLoginScreen.kt
@@ -1,16 +1,40 @@
 package eu.tuto.readabook.screens.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import eu.tuto.readabook.components.ReadLogo
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.sp
+import com.google.api.LogDescriptor
+import eu.tuto.readabook.components.EmailInput
+import eu.tuto.readabook.components.PasswordInput
 
 @Composable
 fun LoginScreen(navController: NavHostController) {
@@ -20,14 +44,74 @@ fun LoginScreen(navController: NavHostController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReadLogo()
+            UserForm(
+                loading = false,
+                isCreateAccount = false
+            ) { email, password ->
+                Log.d("Form", "LoginScreen: $email $password")
+            }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun UserForm(modifier: Modifier = Modifier){
-    val email = rememberSaveable {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwdVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwdFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+    Column(
+        modifier = Modifier
+            .height(250.dp)
+            .background(MaterialTheme.colors.background)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
+            passwdFocusRequest.requestFocus()
+        })
+        PasswordInput(modifier = Modifier.focusRequester(passwdFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwdVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            })
+        SubmitButton(
+            textId = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid,
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+        }
+    }
+}
 
+@Composable
+fun SubmitButton(textId: String, loading: Boolean, validInputs: Boolean, onClick: () -> Unit) {
+    Button(
+        onClick = onClick, modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) {
+            CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        } else {
+            Text(text = textId, modifier = Modifier.padding(5.dp))
+        }
     }
 }
