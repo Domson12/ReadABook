af262d96dbe54c5a7a3e6cf25c1ea0d0d80b454f
Domson12 <dominikmz12@op.pl>
2023-03-11 22:57:04 +0100

    working on updateScreen

--- app/src/main/java/eu/tuto/readabook/screens/update/ReadUpdateScreen.kt
+++ app/src/main/java/eu/tuto/readabook/screens/update/ReadUpdateScreen.kt
@@ -1,14 +1,59 @@
 package eu.tuto.readabook.screens.update
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import eu.tuto.readabook.components.ReaderAppBar
+import eu.tuto.readabook.data.DataOrException
+import eu.tuto.readabook.model.Book
+import eu.tuto.readabook.model.MBook
 import eu.tuto.readabook.screens.Home.HomeViewModel
+import timber.log.Timber
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun UpdateScreen(
     navController: NavHostController,
     bookItemId: String,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
+    Scaffold(topBar = {
+        ReaderAppBar(
+            title = "UpdateBook", icon = Icons.Default.ArrowBack,
+            showProfile = false,
+            navController = navController
+        )
+    }) {
+        val bookInfo = produceState<DataOrException<List<MBook>, Boolean, Exception>>(
+            initialValue = DataOrException(data = emptyList(), true)
+        ) {
+            value = viewModel.data.value
+        }.value
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(3.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(3.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Timber.tag("info").d(viewModel.data.value.data.toString())
+        }
+    }
 }
